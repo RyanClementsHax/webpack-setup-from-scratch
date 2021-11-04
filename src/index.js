7b3cdead92c4d8d0fc869ac8f02059b6aa401bec
Ryan Clements <ryanclementshax@gmail.com>
2021-11-04 14:42:52 -0400

    add example of something that needs to be transpiled

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import getClasses from "./getClasses";
 
 console.log("hello!");
 getClasses();
+
+const obj = { a: "alpha", b: "bravo" };
+const newObj = { ...obj, c: "charlie" };
+console.log(newObj);
