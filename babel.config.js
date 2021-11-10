eed0df2f7baecd797040cf938f8d9ed8b4bb7a27
Ryan Clements <ryanclementshax@gmail.com>
2021-11-10 14:52:50 -0500

    convert .babelrc to babel.config.js and add note on why @babel/preset-react isnt needed

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      { debug: true, useBuiltIns: "usage", corejs: 3 },
+      // @babel/preset-react is not needed because typescript takes care of jsx transpilation for us
+      // https://www.typescriptlang.org/tsconfig#jsx
+    ],
+  ],
+};
